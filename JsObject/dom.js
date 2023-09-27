453e10afd8f724a31c126a5fe4a65dcc7015d62a
unknown <jagadishmunavalli35@gmail.com>
2023-09-28 00:06:58 +0530

    commit

--- JsObject/dom.js
+++ JsObject/dom.js
@@ -12,23 +12,47 @@
 // let res2=document.getElementById("max")
 // console.log(res2); //null
 
-//^ 2. documentquerySelector("Selector")
+//^ 2. document.querySelector("Selector")
 //& gets the element based on the selectors, and gets the first occured element
 
-let res=document.querySelector("a")
-console.log(res); //<a href="" id="demo">Bye</a>
+// let res=document.querySelector("a")
+// console.log(res); //<a href="" id="demo">Bye</a>
 
-let res1=document.querySelector("h1")
-console.log(res1); //<h1 class="demo">Heading</h1>
+// let res1=document.querySelector("h1")
+// console.log(res1); //<h1 class="demo">Heading</h1>
 
 // let res2=document.querySelector(".a")
 // console.log(res2); //<h1 class="a">Hello</h1>
 
-let res3=document.querySelector("#a")
-console.log(res3); //null
+// let res3=document.querySelector("#a")
+// console.log(res3); //null
 
-let res4=document.querySelector("div h2")
-console.log(res4); //
+// let res4=document.querySelector("div h2")
+// console.log(res4); //<h2>h2 inside div</h2>
 
-let res5=document.querySelector("demo")
-console.log(res5); //
+// let res5=document.querySelector("demo")
+// console.log(res5); //null
+
+//^ document.querySelectorALL:-
+//& gets all the elements based on the selector, and returns all elements with the given selector 
+
+let res=document.querySelectorAll("a")
+console.log(res); //NodeList [a#demo]
+
+let res1=document.querySelectorAll("h1")
+console.log(res1); //NodeList(2) [h1.demo, h1.a]
+console.log(res1[0]);//<h1 class="demo">Heading</h1>
+
+let res2=document.querySelectorAll(".a")
+console.log(res2); //NodeList(2) [h1.a, span.a]
+
+let res3=document.querySelectorAll("#a")
+console.log(res3); //NodeList []
+
+let res4=document.querySelectorAll("div h2")
+console.log(res4); //NodeList [h2]
+
+let res5=document.querySelectorAll("demo")
+console.log(res5); //null
+
+
