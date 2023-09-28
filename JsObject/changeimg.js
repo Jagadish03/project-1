2023237b165ec8304e3db3a05996b3ce9cfe8a56
unknown <jagadishmunavalli35@gmail.com>
2023-09-28 22:23:07 +0530

    commit

---
+++ JsObject/changeimg.js
@@ -0,0 +1,21 @@
+function img1()
+{
+    let image=document.getElementById("image");
+    image.src="https://cdn.pixabay.com/photo/2017/03/27/12/50/flower-2178507__340.jpg"
+}
+function img2()
+{
+    let image=document.getElementById("image");
+    image.src="https://cdn.pixabay.com/photo/2017/03/27/12/50/flower-2178507__640.jpg"
+}
+function img3()
+{
+    let image=document.getElementById("image");
+    image.src="https://cdn.pixabay.com/photo/2023/09/05/12/29/boy-8235025_640.jpg"
+}
+function img4()
+{
+    let image=document.getElementById("image");
+    image.src="https://cdn.pixabay.com/photo/2023/09/16/10/10/kingfisher-8256375_640.jpg"
+}
+
